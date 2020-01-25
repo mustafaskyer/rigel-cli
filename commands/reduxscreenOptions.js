69579cd002151915f1cbe483a59fb388b1ad598b
Mustafa Elsayed <mskyer@protonmail.com>
2020-01-26 00:28:43 +0300

    updated  ✅

--- commands/reduxscreenOptions.js
+++ commands/reduxscreenOptions.js
@@ -10,7 +10,7 @@ import {
 } from '../lib/utils'
 import { map } from 'lodash'
 
-async function createScreen (name, comps) {
+async function createScreen(name, comps) {
   // create new directory in App/Screens path
   const createdDir = await createDir('App/screens', name)
   if (createdDir) {
@@ -30,7 +30,7 @@ async function createScreen (name, comps) {
   }
 }
 
-async function createAction (name, casses) {
+async function createAction(name, casses) {
   const impCasesStmt = casses.join(', ')
   const createdAction = await createFile('App/redux/actions', `${name}`)
   const onlineActionContent = await getOnlineFileContent('action')
@@ -39,36 +39,35 @@ async function createAction (name, casses) {
   await importInto(`App/redux/actions/${name}.js`, `import { ${impCasesStmt} } from 'redux-types'`, ';')
   await handle.appendToFile(
     ';',
-        `export * from './${name}'`,
-        actionIndexPath.path,
-        { flag: 'a' }
+    `export * from './${name}'`,
+    actionIndexPath.path,
   )
 }
 
-async function createSaga (name, casses) {
+async function createSaga(name, casses) {
   const impCasesStmt = casses.join(', ')
   const onlineActionContent = await getOnlineFileContent('saga')
   const wUpdated = await onlineActionContent.replace('watch()', `watch${name}()`)
   const sUpdated = await wUpdated.replace('SUCCESS', casses[0])
   const fUpdated = await sUpdated.replace('FAILED', casses[1])
   const tUpdated = await fUpdated.replace('//type', casses[2])
-  const weUpdated = await tUpdated.replace('//fn', 'fn')
+  const weUpdated = await tUpdated.replace('//handle', `handle`)
   await handle.writeFileSync(`App/redux/sagas/${name}.js`, JSON.parse(weUpdated))
   const sagasIndexPath = await handle.getPath('App/redux/sagas/index.js')
   await importInto(`App/redux/sagas/${name}.js`, `import { ${impCasesStmt} } from 'redux-types'`, ';')
   await handle.appendToFile(
     ';',
-        `import { watch${name} } from './${name}'`,
-        sagasIndexPath.path
+    `import watch${name} from './${name}'`,
+    sagasIndexPath.path
   )
   await handle.appendToFile(
     'yield all([',
-        `fork(watch${name}),`,
-        sagasIndexPath.path
+    `fork(watch${name}),`,
+    sagasIndexPath.path
   )
 }
 
-async function createReducer (name, casses) {
+async function createReducer(name, casses) {
   const impCasesStmt = casses.join(', ')
   const createdReducer = await createFile('App/redux/reducers', `${name}Reducer`)
   const onlineFile = await getOnlineFileContent('reducer')
@@ -76,18 +75,18 @@ async function createReducer (name, casses) {
   const reducerIndexPath = await handle.getPath('App/redux/reducers/index.js')
   await importInto(`App/redux/reducers/${name}Reducer.js`, `import { ${impCasesStmt} } from 'redux-types'`, ';')
   await handle.appendToFile(
-    'from "redux";', // occurance
-        `import ${name.toLowerCase()} from './${name}Reducer';`, // value to replace
-        reducerIndexPath.path // path of file
+    ';', // occurance
+    `import ${name.toLowerCase()} from './${name}Reducer';`, // value to replace
+    reducerIndexPath.path // path of file
   )
   await handle.appendToFile(
     'combineReducers({', // occurance
-        `${name.toLowerCase()},`, // value to replace
-        reducerIndexPath.path // path of file
+    `${name.toLowerCase()},`, // value to replace
+    reducerIndexPath.path // path of file
   )
 }
 
-async function createTypes (name) {
+async function createTypes(name) {
   const createdTypes = await createFile('App/redux/types', name)
   console.log('@createdTypes', createdTypes)
   const onlineTypesContent = await getOnlineFileContent('type')
@@ -98,17 +97,17 @@ async function createTypes (name) {
   const typesIndexPath = await handle.getPath('App/redux/types/index.js')
   await handle.appendToFile(
     ';',
-        `\n export * from './${name}';`,
-        typesIndexPath.path
+    `\n export * from './${name}';`,
+    typesIndexPath.path
   )
   return [
-        `${name.toUpperCase()}_CASE_SUCCESS`,
-        `${name.toUpperCase()}_CASE_FAILED`,
-        `${name.toUpperCase()}_CASE`
+    `${name.toUpperCase()}_CASE_SUCCESS`,
+    `${name.toUpperCase()}_CASE_FAILED`,
+    `${name.toUpperCase()}_CASE`
   ]
 }
 
-export default async function handleCreateReduxScreen (name, shouldImportComponents) {
+export default async function handleCreateReduxScreen(name, shouldImportComponents) {
   /**
      * - should create screen with index, style files
      * - should create action & export from index
