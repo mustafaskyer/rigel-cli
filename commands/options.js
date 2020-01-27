197d320d82e4193bf6a9f1672ddbe1502cf49c6f
Mustafa Elsayed <mskyer@protonmail.com>
2020-01-28 01:02:52 +0300

    fixed adding issue

--- commands/options.js
+++ commands/options.js
@@ -6,22 +6,22 @@ export default async function checkOptions (progrm) {
   const componentsPath = await handle.getPath('App/Components')
   const actionsPath = await handle.getPath('App/redux/actions')
   return new Promise((resolve, reject) => {
+
     if (progrm.imc) {
       ls(componentsPath.path, (files) => {
         getFilesList(files, 'Components', (selectedComps) => resolve(selectedComps))
       })
       return
-    }
-
-    if (progrm.ima) {
+    } else if (progrm.ima) {
       ls(actionsPath.path, (files) => {
         getFilesList(files, 'Actions', (selectedActions) => resolve(selectedActions))
       })
-    }
-
-    if (progrm.export) {
+    }else if (progrm.export) {
       console.log('@exporting to ...')
       getSelectedPath((selectedPaths) => resolve(selectedPaths))
+    }else {
+      resolve(true)
     }
+
   })
 }
