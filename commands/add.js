1773d0886ee9e6a04790cd97dba90913712b0aa8
Mustafa Elsayed <mskyer@protonmail.com>
2020-04-05 13:44:18 +0300

    [UPDATED] handled --connect ✅

--- commands/add.js
+++ commands/add.js
@@ -1,35 +1,44 @@
+const inquirer = require('inquirer');
 
-const inquirer = require('inquirer')
-
-const { getAnswers } = require('../lib/add')
+// const { handleOptions } = require('../lib/add')
+const { handleOptions } = require('../lib/add/index');
+const { CONNECT_ALL } = require('../lib/types');
 
 const types = [
-  { name: 'screen', value: 'scr' },
-  { name: 'component', value: 'comp' },
-  // { name: 'style', value: 'style' },
-  { name: 'reducer', value: 'reducer' },
-  { name: 'reducer action', value: 'reducer_action' },
-  { name: 'reducer types', value: 'reducer_types' },
-  { name: 'reducer sagas', value: 'reducer_sagas' }
-]
+	{ name: 'screen', value: 'scr' },
+	{ name: 'component', value: 'comp' },
+	// { name: 'style', value: 'style' },
+	{ name: 'reducer', value: 'reducer' },
+	{ name: 'reducer action', value: 'reducer_action' },
+	{ name: 'reducer types', value: 'reducer_types' },
+	{ name: 'reducer sagas', value: 'reducer_sagas' }
+];
 
-function validate (answer) {
-  if (answer.length < 1) {
-    return 'You must choose at least one topping.'
-  }
+function validate(answer) {
+	if (answer.length < 1) {
+		return 'You must choose at least one topping.';
+	}
 
-  return true
+	return true;
 }
-const viewQuestions = (name, obj) => {
-  inquirer.prompt([{
-    type: 'list',
-    message: 'Choose File Type',
-    name: 'selected',
-    choices: types,
-    validate
-  }]).then(listAnswers => {
-    getAnswers(listAnswers, name, obj)
-  })
-}
+const viewOptions = (name, options) => {
+	if (options.includes('--connect')) {
+		handleOptions({ selected: CONNECT_ALL }, name, options);
+		return;
+	}
+	inquirer
+		.prompt([
+			{
+				type: 'list',
+				message: 'Choose File Type',
+				name: 'selected',
+				choices: types,
+				validate
+			}
+		])
+		.then((selected) => {
+			handleOptions(selected, name, options);
+		});
+};
 
-module.exports = { viewQuestions }
+module.exports = { viewOptions };
