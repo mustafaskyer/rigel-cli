4090a06bdea38262a09b285fbd2467145d92c87c
Mustafa Elsayed <mskyer@protonmail.com>
2021-01-12 23:08:35 +0300

    fixed catch issues

--- packages/utils/find_path.util.js
+++ packages/utils/find_path.util.js
@@ -1,9 +1,11 @@
 const PrettyError = require('pretty-error');
 const jetpack = require('fs-jetpack')
+const shell = require('shelljs')
 const pe = new PrettyError();
 
-const count = 0;
+let count = 0;
 function findPath(path) {
+
 	try {
 		if (count > 5) {
 			return { status: false, message: "make sure you're inside arigel app" };
