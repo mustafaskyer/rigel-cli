f7d59fcf04331ad4c698455625cb1af6c6c56c4c
Mustafa Elsayed <mskyer@protonmail.com>
2021-10-12 13:33:45 +0300

    getting platform configuration

--- src/utils/find_path.util.ts
+++ src/utils/find_path.util.ts
@@ -1,26 +1,37 @@
-import PrettyError from "pretty-error";
-import jetpack from "fs-jetpack";
-const shell = require("shelljs");
+import PrettyError from 'pretty-error';
+import jetpack from 'fs-jetpack';
+const shell = require('shelljs');
 const pe = new PrettyError();
 
 let count = 0;
-export function findPath(
-  path: string
-): { status?: boolean; message?: string; path?: string } | any {
+export function findPath(path: string): { status?: boolean; message?: string; path?: string } | any {
   try {
     if (count > 5) {
-      return { status: false, message: "make sure you're inside arigel app" };
+      return { status: false, message: "make sure you're inside a react-native app with the rigel structure" };
     }
 
-    if (jetpack.exists(".rigel")) {
+    if (jetpack.exists('.rigel')) {
+      let isTypeScript = jetpack.exists('tsconfig.json');
+      let isMobile;
+      let isWeb;
+      if (jetpack.exists('android') && jetpack.exists('ios')) {
+        isMobile = true;
+      } else {
+        if (jetpack.exists('public/index.html')) {
+          isWeb = true;
+        }
+      }
       return {
         status: true,
+        typescript: isTypeScript,
+        mobile: isMobile,
+        web: isWeb,
         path: `${process.cwd()}/App/${path}`,
-        message: "success",
+        message: 'success',
       };
     }
 
-    shell.cd("../");
+    shell.cd('../');
     count++;
 
     return findPath(path);
