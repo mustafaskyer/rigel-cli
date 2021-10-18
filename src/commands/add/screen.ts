66ee864a213e14bea0c258075a0cc23d49a47e37
Mustafa Elsayed <mskyer@protonmail.com>
2021-10-18 16:25:50 +0300

    updated with components content & screen

--- src/commands/add/screen.ts
+++ src/commands/add/screen.ts
@@ -1,4 +1,5 @@
 import { findPath, checkIfExist, getLsAnswers, gDir, gFile, appendToFile } from '../../utils/index';
+import { componentTypeScript, compRnSource } from './templates/component.temp';
 const { compile } = require('handlebars');
 const ora = require('ora');
 const shell = require('shelljs');
@@ -6,8 +7,7 @@ const PrettyError = require('pretty-error');
 
 const pe = new PrettyError();
 
-import { screenRnSource } from './templates/screen.temp';
-import { stylesRnSources } from './templates/styles.temp';
+import { stylesRnSources, stylesTypeScript } from './templates/styles.temp';
 
 export const addScreen = async (
   name: string,
@@ -36,54 +36,61 @@ export const addScreen = async (
   }
 
   // all new options should handle from here
-  const { status, path, message } = findPath('screens');
+  const { status, path, typescript, mobile, web } = findPath('screens');
   if (!status) {
     console.log(pe.render(`something unexpected happened!!`));
     process.exit();
   }
-  const checkStatus = await checkIfExist(path, name, 'screen');
+  const checkStatus = await checkIfExist(path, name, 'screen', typescript);
   if (checkStatus) {
     console.log(pe.render(`${name} already exist, can't add ${name} directory!!`));
     process.exit();
   }
 
-  const sourcetemplate = compile(screenRnSource);
-  const sourcetemplateStyles = compile(stylesRnSources);
-  const _content = sourcetemplate({ name, date: new Date() });
-  const _contentStyle = sourcetemplateStyles({ name, date: new Date() });
+  const sContent = compile(typescript ? componentTypeScript : compRnSource);
+  const sStyles = compile(typescript ? stylesTypeScript : stylesRnSources);
+  const _content = sContent({ name, date: new Date() });
+  const _contentStyle = sStyles({ name, date: new Date() });
   const dirCreated = await gDir(path, name);
+
+  const extReact = typescript ? 'tsx' : 'js';
+  const ext = typescript ? 'ts' : 'js';
 
   if (!dirCreated) {
     console.log(pe.render(`${name} already exist, can't add ${name} directory!!`));
     process.exit();
   }
 
-  gFile({ path: `${path}/${name}`, name, type: 'screen', content: _content });
+  gFile({ path: `${path}/${name}`, name, type: 'screen', content: _content, ext: extReact });
   gFile({
     path: `${path}/${name}`,
     name,
     type: 'styles',
     content: _contentStyle,
+    ext: ext,
   });
 
   // should check opts and add imc, imac answers if exists :-
   if (opts.imc) {
     const o = opts.imc.join(', ');
-    appendToFile(`${path}/${name}/${name}.screen.js`, [';'], `import { ${o} } from '@components'`);
+    appendToFile(`${path}/${name}/${name}.screen.${extReact}`, [';'], `import { ${o} } from 'components'`);
   } else {
-    appendToFile(`${path}/${name}/${name}.screen.js`, [';'], `import { Spacer, Row } from '@components'`);
+    appendToFile(`${path}/${name}/${name}.screen.${extReact}`, [';'], `import { Spacer, Row } from 'components'`);
   }
 
   if (opts.imac) {
-    appendToFile(`${path}/${name}/${name}.screen.js`, [';'], `import {} from '@actions'`);
+    appendToFile(`${path}/${name}/${name}.screen.${extReact}`, [';'], `import {} from 'actions'`);
   }
 
   spin.succeed(` - successfully added ${name} @ App/components/${name}.component`);
-  spin.info(` - usage: import { ${name} } from '@components'; `);
+  spin.info(` - usage: import { ${name} } from 'components'; `);
 
-  shell.exec(`code App/screens/${name}/${name}.screen.js`, (code: string | number, _stdout: any, _stderr: any) => {
-    if (code === 0) {
-      spin.info('file opened at vscode ✅');
-    }
-  });
+  shell.exec(
+    `code App/screens/${name}/${name}.screen.${extReact}`,
+    (code: string | number, _stdout: any, _stderr: any) => {
+      if (code === 0) {
+        spin.info('file opened at vscode ✅');
+      }
+    },
+  );
 };
